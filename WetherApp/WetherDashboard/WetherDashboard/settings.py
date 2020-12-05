dc0d1d1ac526d4c79ca4bb3e17400eb9211f2b31
Ritik Soni <47344024+ritiksoni00@users.noreply.github.com>
2020-12-05 08:58:16 +0530

    Update settings.py

--- WetherApp/WetherDashboard/WetherDashboard/settings.py
+++ WetherApp/WetherDashboard/WetherDashboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!z6t%b_n*c14f@dnqf=t-d-mil(cq^17dwon&l)$789h6gvjz6'
+SECRET_KEY = '--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
